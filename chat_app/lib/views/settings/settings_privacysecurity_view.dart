fee1f789458ad5b542221a42609e022ec6523022
Ajah Emmanuel Nnanna <emmanuel.fmrs.marach@gmail.com>
2024-03-05 23:14:56 +0100

    Formatted the remaining part of settings_privacysecurity_view

--- chat_app/lib/views/settings/settings_privacysecurity_view.dart
+++ chat_app/lib/views/settings/settings_privacysecurity_view.dart
@@ -120,60 +120,93 @@ class PASView extends State<PrivacyAndSecurityView>{
                 padding: EdgeInsets.only(left: 40),
                 child: Align(
                   alignment: Alignment.centerLeft,
-                  child: CustomTextWidget(size: 13, fontWeight: FontWeight.w600, color: customGreenColor, text: 'Calls')
+                  child: CustomTextWidget(
+                    size: 13,
+                    fontWeight: FontWeight.w600,
+                    color: customGreenColor,
+                    text: 'Calls'
+                  )
                 ),
               ),
               Gap(5),
               CustomListTileWithSubtitle( 
-                title: 'Who can call me?', subtitle: 'Everyone',
-                topBorderRadius: 0, bottomBorderRadius: 0, sizeOfSubtitle: 13,
+                title: 'Who can call me?',
+                subtitle: 'Everyone',
+                topBorderRadius: 0,
+                bottomBorderRadius: 0,
+                sizeOfSubtitle: 13,
               ),
               Gap(20),
               Padding(
                 padding: EdgeInsets.only(left: 40),
                 child: Align(
                   alignment: Alignment.centerLeft,
-                  child: CustomTextWidget(size: 13, fontWeight: FontWeight.w600, color: customGreenColor, text: 'Chats')
+                  child: CustomTextWidget(
+                    size: 13,
+                    fontWeight: FontWeight.w600,
+                    color: customGreenColor,
+                    text: 'Chats'
+                  )
                 ),
               ),
               Gap(10),
               CustomListTileWithSwitch( 
-                title: 'Disable Forwarded', subtitle: 'Allows you to re-send messages without Forwarding tag',
-                topBorderRadius: 0, bottomBorderRadius: 0, sizeOfSubtitle: 13,
+                title: 'Disable Forwarded',
+                subtitle: 'Allows you to re-send messages without Forwarding tag',
+                topBorderRadius: 0,
+                bottomBorderRadius: 0,
+                sizeOfSubtitle: 13,
               ),
               Gap(5),
               CustomDivider(indent: 40),
               Gap(5),
               CustomListTileWithSwitch( 
-                title: 'Anti-Delete Messages', subtitle: 'Other people cannot delete messages for you',
-                topBorderRadius: 0, bottomBorderRadius: 0, sizeOfSubtitle: 13,
+                title: 'Anti-Delete Messages',
+                subtitle: 'Other people cannot delete messages for you',
+                topBorderRadius: 0,
+                bottomBorderRadius: 0,
+                sizeOfSubtitle: 13,
               ),
               Gap(5),
               CustomDivider(indent: 40),
               Gap(5),
               CustomListTileWithSwitch( 
-                title: 'Show Blue Ticks after reply', subtitle: 'Contact will only see blue ticks after you reply',
-                topBorderRadius: 0, bottomBorderRadius: 0, sizeOfSubtitle: 13,
+                title: 'Show Blue Ticks after reply',
+                subtitle: 'Contact will only see blue ticks after you reply',
+                topBorderRadius: 0,
+                bottomBorderRadius: 0,
+                sizeOfSubtitle: 13,
               ),
               Gap(20),
               Padding(
                 padding: EdgeInsets.only(left: 40),
                 child: Align(
                   alignment: Alignment.centerLeft,
-                  child: CustomTextWidget(size: 13, fontWeight: FontWeight.w600, color: customGreenColor, text: 'Status')
+                  child: CustomTextWidget(
+                    size: 13,
+                    fontWeight: FontWeight.w600,
+                    color: customGreenColor,
+                    text: 'Status'
+                  )
                 ),
               ),
               Gap(10),
               CustomListTileWithSwitch( 
-                title: 'Hide View Status', subtitle: "Don't tell contact that you have viewed their status",
-                topBorderRadius: 0, bottomBorderRadius: 0, sizeOfSubtitle: 13,
+                title: 'Hide View Status',
+                subtitle: "Don't tell contact that you have viewed their status",
+                topBorderRadius: 0,
+                bottomBorderRadius: 0,
+                sizeOfSubtitle: 13,
               ),
               Gap(5),
               CustomDivider(indent: 40),
               Gap(5),
               CustomListTileWithSwitch( 
-                title: 'Anti-Delete Status', subtitle: 'Deleted statuses/stories will not be deleted for you.',
-                topBorderRadius: 0, bottomBorderRadius: 0, sizeOfSubtitle: 13,
+                title: 'Anti-Delete Status',
+                subtitle: 'Deleted statuses/stories will not be deleted for you.',
+                topBorderRadius: 0,
+                bottomBorderRadius: 0,
+                sizeOfSubtitle: 13,
               ),
             ]
           ),
@@ -187,37 +220,54 @@ class PASView extends State<PrivacyAndSecurityView>{
                 padding: EdgeInsets.only(left:30),
                 child: Align(
                   alignment: Alignment.centerLeft,
-                  child: CustomTextWidget(size: 15, fontWeight: FontWeight.w600, text: 'SECURITY', color: customWhiteColor)
+                  child: CustomTextWidget(
+                    size: 15,
+                    fontWeight: FontWeight.w600,
+                    text: 'SECURITY',
+                    color: customWhiteColor
+                  )
                 ),
               ),
               CustomDivider(indent: 30),
               CustomListTileWithSubtitle(
-                title: 'Change Password', subtitle: 'Default Password is: 0000, Change it to yours!',
-                topBorderRadius: 0, bottomBorderRadius: 0, sizeOfSubtitle: 13,
+                title: 'Change Password',
+                subtitle: 'Default Password is: 0000, Change it to yours!',
+                topBorderRadius: 0,
+                bottomBorderRadius: 0,
+                sizeOfSubtitle: 13,
               ),
               CustomDivider(indent: 30),
               CustomListTileWithoutSubtitle(
                 title: 'Change Pattern',
-                topBorderRadius: 0, bottomBorderRadius: 0,
+                topBorderRadius: 0,
+                bottomBorderRadius: 0,
               ),
               CustomDivider(indent: 30),
               CustomListTileWithoutSubtitle(
                 title: 'Recovery Question',
-                topBorderRadius: 20, bottomBorderRadius: 0,
+                topBorderRadius: 20,
+                bottomBorderRadius: 0,
               ),
               CustomDivider(indent: 30),
               CustomListTileWithoutSubtitle(
                 title: 'Change Change Lock/pattern Wallpaper,',
-                topBorderRadius: 0, bottomBorderRadius: 0,
+                topBorderRadius: 0,
+                bottomBorderRadius: 0,
               ),
               CustomDivider(indent: 30),
               CustomListTileWithSwitch(
-                title: 'Make Pattern Invisible', subtitle: '',
-                topBorderRadius: 0, bottomBorderRadius: 0, sizeOfSubtitle: 13,
+                title: 'Make Pattern Invisible',
+                subtitle: '',
+                topBorderRadius: 0,
+                bottomBorderRadius: 0,
+                sizeOfSubtitle: 13,
               ),
               CustomListTileWithSwitch(
-                title: 'Disable pattern vibration', subtitle: '',
-                topBorderRadius: 0, bottomBorderRadius: 0, sizeOfSubtitle: 13,
+                title: 'Disable pattern vibration',
+                subtitle: '',
+                topBorderRadius: 0,
+                bottomBorderRadius: 0,
+                sizeOfSubtitle: 13,
               )
             ]
           )
